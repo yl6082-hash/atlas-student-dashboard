4cae27f1ca81a908a5fabc2e9340c1848e4275e2
yl6082-hash <yl6082@nyu.edu>
2026-07-28 11:41:32 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "07f147fc-d074-4ace-89ee-73638e37c224";
 
 const { d1, r2 } = hostingConfig;
 
